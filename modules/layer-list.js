3002cc36b88cc08f219a371c9ccd2e86b73d1842
Fernando Gonzalez <fernando.gonzalez@geomati.co>
2013-10-07 13:16:53 +0200

    Layer groups

--- modules/layer-list.js
+++ modules/layer-list.js
@@ -1,9 +1,6 @@
 define([ "jquery" ], function($) {
 
-	/*
-	 * One of this for each group in a jquery ui accordion
-	 */
-	var tblLayerGroup = null;
+	var divLayers = null;
 
 	$(document).trigger("css-load", "modules/layer-list.css");
 
@@ -47,38 +44,53 @@ define([ "jquery" ], function($) {
 		divActiveLayers.append(h3Title);
 		divLayersContainer.append(divActiveLayers);
 
-		var divLayers = $("<div/>").attr("id", "all_layers");
-		tblLayerGroup = $("<table/>").attr("id", "group-content-table");
-		divLayers.append(tblLayerGroup);
+		divLayers = $("<div/>").attr("id", "all_layers");
+		divLayers.accordion({
+			"animate" : false
+		});
 		divLayersContainer.append(divLayers);
 	});
-
+	$(document).bind("add-group", function(event, groupInfo) {
+		console.log(groupInfo);
+		console.log(groupInfo.id);
+		var h3Title = $("<h3/>").html(groupInfo.name);
+		divLayers.append(h3Title);
+		var tblLayerGroup = $("<table/>");
+		tblLayerGroup.attr("id", "group-content-table-" + groupInfo.id);
+		tblLayerGroup.addClass("group-content-table");
+		divLayers.append(tblLayerGroup).accordion("refresh");
+	});
 	$(document).bind("add-layer", function(event, layerInfo) {
-		var trLayer = $("<tr/>").addClass("layer_row");
+		var tblLayerGroup = $("#group-content-table-" + layerInfo.groupId);
+		if (tblLayerGroup.length == 0) {
+			$(document).trigger("error", "Layer " + layerInfo.name + " references nonexistent group: " + layerInfo.groupId);
+		} else {
+			var trLayer = $("<tr/>").addClass("layer_row");
 
-		var tdLegend = $("<td/>").addClass("layer_legend");
-		trLayer.append(tdLegend);
+			var tdLegend = $("<td/>").addClass("layer_legend");
+			trLayer.append(tdLegend);
 
-		var tdVisibility = $("<td/>").css("width", "16px");
-		var divCheckbox = $("<div/>").addClass("layer_visibility");
-		emulate(divCheckbox);
-		tdVisibility.append(divCheckbox);
+			var tdVisibility = $("<td/>").css("width", "16px");
+			var divCheckbox = $("<div/>").addClass("layer_visibility");
+			emulate(divCheckbox);
+			tdVisibility.append(divCheckbox);
 
-		trLayer.append(tdVisibility);
+			trLayer.append(tdVisibility);
 
-		var tdName = $("<td/>").addClass("layer_name");
-		tdName.html(layerInfo.name);
-		trLayer.append(tdName);
+			var tdName = $("<td/>").addClass("layer_name");
+			tdName.html(layerInfo.name);
+			trLayer.append(tdName);
 
-		var tdInfo = $("<td/>").addClass("layer_info");
-		if (layerInfo.hasOwnProperty("infoLink")) {
-			var aLink = $("<a/>").attr("href", layerInfo.infoLink);
-			aLink.addClass("layer_info_button");
-			tdInfo.append(aLink);
+			var tdInfo = $("<td/>").addClass("layer_info");
+			if (layerInfo.hasOwnProperty("infoLink")) {
+				var aLink = $("<a/>").attr("href", layerInfo.infoLink);
+				aLink.addClass("layer_info_button");
+				tdInfo.append(aLink);
+			}
+			trLayer.append(tdInfo);
+
+			tblLayerGroup.append(trLayer);
 		}
-		trLayer.append(tdInfo);
-
-		tblLayerGroup.append(trLayer);
 	});
 
 });
--- modules/main.js
+++ modules/main.js
@@ -8,32 +8,42 @@ require.config({
 	}
 });
 
-require([ "jquery", "iso8601", "css-loader", "layout", "customization" ], function($) {
+require([ "jquery", "iso8601", "css-loader", "layout", "customization", "error-management" ], function($) {
 	$(document).trigger("css-load", "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.min.css");
 	/*
 	 * Queries the server and launches add-group and add-layer events
 	 */
+	$(document).trigger("add-group", {
+		"id" : "basic",
+		"name" : "Basic layers"
+	});
+	$(document).trigger("add-group", {
+		"id" : "spain",
+		"name" : "Spain"
+	});
 	$(document).trigger("add-layer", {
+		"groupId" : "basic",
 		"url" : "http://vmap0.tiles.osgeo.org/wms/vmap0",
 		"wmsName" : "basic",
 		"name" : "Countries",
 		"timestamps" : [ "2000", "2003" ]
 	});
-	$(document).trigger("add-layer", {
-		"url" : "http://www.cartociudad.es/wms/CARTOCIUDAD/CARTOCIUDAD?",
-		"wmsName" : "FondoUrbano",
-		"name" : "Regions of Spain",
-		"infoLink" : "http://rdc-snsf.org/static/loc/en/html/bluemarble_def.html",
-		"timestamps" : [ "2000", "2005" ]
-	});
 	// $(document).trigger("add-layer", {
-	// "url" : "http://ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx",
-	// "wmsName" : "Catastro",
-	// "name": "Cadastrial data of Spain",
+	// "url" : "http://www.cartociudad.es/wms/CARTOCIUDAD/CARTOCIUDAD?",
+	// "wmsName" : "FondoUrbano",
+	// "name" : "Regions of Spain",
 	// "infoLink" :
 	// "http://rdc-snsf.org/static/loc/en/html/bluemarble_def.html",
 	// "timestamps" : [ "2000", "2005" ]
 	// });
+	$(document).trigger("add-layer", {
+		"groupId" : "spain",
+		"url" : "http://ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx",
+		"wmsName" : "Catastro",
+		"name" : "Cadastrial data of Spain",
+		"infoLink" : "http://rdc-snsf.org/static/loc/en/html/bluemarble_def.html",
+		"timestamps" : [ "2000", "2005" ]
+	});
 
 	$(document).trigger("initial-zoom");
 });
